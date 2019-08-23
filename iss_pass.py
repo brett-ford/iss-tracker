9743063be0dd3d05206b26c44e653f9c90cd7989
Brett Ford <bford@US-BFords-MacBook-Pro.local>
2019-08-22 21:42:46 -0400

    First iss  commit

---
+++ iss_pass.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime as dt
+
+"""
+Finds the next 5 times the International Space Station will fly over a location. 
+"""
+
+mb = {"name": "Moses Brown School", "arguments": {"lat": 41.8334, "lon": -71.3985, "alt": 43, "n": 5}}
+mb_parameters = mb["arguments"]
+
+iss_response = requests.get("http://api.open-notify.org/iss-pass.json", params=mb_parameters)
+iss = iss_response.json()
+
+print("***** ISS Flyover Times *****")
+print("Current Time:")
+print(dt.today().strftime("%Y-%m-%d %H:%M:%S"))
+
+# Neat printout of response.
+for key in ['message', 'request']:
+    print("{}: {}".format(key, iss[key]))
+for response in iss['response']:
+    print(response)
+
+# Summary of response.
+print("Next 5 ISS flyover times for {}:".format(mb["name"]))
+for response in iss["response"]:
+    flyover = str(dt.fromtimestamp(response["risetime"])) + " for " + str(response["duration"]) + " seconds."
+    print(flyover)
+
+print("***** Finished *****")
