573269efeb70948c152779fb7542a691b46760f2
Brett Ford <brettjford@gmail.com>
2021-05-16 18:31:51 -0400

    1. Updated iss_pass.py

--- iss_pass.py
+++ iss_pass.py
@@ -1,5 +1,4 @@
 import requests
-
 from datetime import datetime as dt
 
 """
@@ -7,26 +6,25 @@ Finds the next 5 times the International Space Station will fly over a location.
 Visible 10 degrees above the horizon. 
 """
 
-mb = {"name": "Moses Brown School", "arguments": {"lat": 41.8334, "lon": -71.3985, "alt": 43, "n": 5}}
-mb_parameters = mb["arguments"]
+mb = {'name': 'Moses Brown School', 'parameters': {'lat': 41.8334, 'lon': -71.3985, 'alt': 43, 'n': 5}}
 
-iss_response = requests.get("http://api.open-notify.org/iss-pass.json", params=mb_parameters)
+iss_response = requests.get('http://api.open-notify.org/iss-pass.json', params=mb['parameters'])
 iss = iss_response.json()
 
-print("****** ISS Flyover Times ******")
-print("Current Time:")
-print(dt.today().strftime("%Y-%m-%d %H:%M:%S"))
+print('****** ISS Flyover Times ******')
+print('Current Time:')
+print(dt.today().strftime('%Y-%m-%d %H:%M:%S'))
 
 # Neat printout of response.
 for key in ['message', 'request']:
-    print("{}: {}".format(key, iss[key]))
+    print('{}: {}'.format(key, iss[key]))
 for response in iss['response']:
     print(response)
 
 # Summary of response.
-print("Next 5 ISS flyover times for {}:".format(mb["name"]))
-for response in iss["response"]:
-    flyover = str(dt.fromtimestamp(response["risetime"])) + " for " + str(response["duration"]) + " seconds."
+print('Next 5 ISS flyover times for {}:'.format(mb['name']))
+for response in iss['response']:
+    flyover = str(dt.fromtimestamp(response['risetime'])) + ' for ' + str(response['duration']) + ' seconds.'
     print(flyover)
 
-print("****** Finished ******")
+print('****** Finished ******')
